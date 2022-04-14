af42318819c8e7d47cb20c9ba0d4e0cba4272967
Nnadi Charis <charisnnadi15@gmail.com>
2022-04-13 17:10:05 -0700

    sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_sign - Prints the sign of a number
+ *
+ * Return: Always 1,0 (Success), otherwise -1
+ */
+
+int print_sign(int n)
+{
+	int n;
+
+	if(n * (_-1) < 0)
+	{
+		putchar('+');
+		return (1);
+	}
+	else if(n * (_-1) > 0)
+	{
+		putchar('-');
+		return (-1);
+	}
+	else
+	{
+		putchar('0');
+		return (0);
+	}
+
+}
