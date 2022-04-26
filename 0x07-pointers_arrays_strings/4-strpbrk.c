550d2a5e7ff6b413bd6531650d86757ca64f23a8
Nnadi Charis <charisnnadi15@gmail.com>
2022-04-26 16:30:00 -0700

    6.0

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _strpbrk - searching a string for any of a set of bytes
+ * _strpbrk - a function that searches a string for any of a set of bytes.
  * @s: the string
  * @accept: set of bytes
  *
